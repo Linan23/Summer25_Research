86b977107ed7ffd7a8cb8b1c556812161a6602da
linan <linan@lafayette.edu>
2025-05-29 23:42:01 -0400

    added a rotation function to the node structure, verified with a few unit test(single,multiple rotations) and all test is valid

---
+++ b_tree.h
@@ -0,0 +1,11 @@
+#ifndef B_TREE_H_
+#define B_TREE_H_
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+
+void build(const std::vector<int>& preorder, const std::vector<int>& inorder);
+
+#endif /*B_TREE_H_*/
